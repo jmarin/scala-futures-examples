6692acf113f9462614eac07b6e1f3b03f1f60e4c
Juan Marin Otero <juan.marin.otero@gmail.com>
2022-10-21 18:01:52 -0400

    Add test stub

---
+++ shared/src/test/scala/com/jmarin/futures/weather/WeatherApiSpec.scala
@@ -0,0 +1,25 @@
+package com.jmarin.futures.weather
+
+import org.scalatest.wordspec.AsyncWordSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.concurrent.ScalaFutures
+
+class WeatherApiSpec extends AsyncWordSpec with Matchers with ScalaFutures:
+
+  "Weather API" should {
+    "predict conditions for Amsterdam" in {
+      pending
+    }
+    "predict conditions for London" in {
+      pending
+    }
+    "predict conditions for Madrid" in {
+      pending
+    }
+    "predict conditions for Rome" in {
+      pending
+    }
+    "predict conditions for Stockholm" in {
+      pending
+    }
+  }
